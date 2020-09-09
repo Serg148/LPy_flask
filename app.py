d244a30b42a45487d58f2ef0f114898133afaf43
Ваше Имя <serg148>
2020-09-09 18:31:59 +0300

    add generater_password

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import Flask
+from flask import Flask, request
+from utils import generater_random_password
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return'Hello, World!'
+    return ' Hello, World!'
+
+@app.route('/generate-password/')
+def generate_password():
+    password_len = request.args.get("password-len", "10")
+    if not password_len.isdigit():
+        return "Error, password-len should be integer"
+
+    password_len = int(password_len)
+
+    if password_len > 100:
+        return "Password should be less then 100"
+
+    return generater_random_password(password_len)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
